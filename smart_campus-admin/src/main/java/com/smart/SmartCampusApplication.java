5af26e7c73a928f5c1bbd05a72acab28a2f50ca3
程安宁 <3308869544@qq.com>
2026-03-11 00:24:53 +0800

    first commit

---
+++ smart_campus-admin/src/main/java/com/smart/SmartCampusApplication.java
@@ -0,0 +1,20 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author can
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class SmartCampusApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SmartCampusApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  AI 赋能智慧校园自主学习生态系统 启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+}
